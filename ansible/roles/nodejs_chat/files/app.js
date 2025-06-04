c7ab94cc4df12a1f34e72913f7e2fce2c4b3ea91
Samuel <samniisowah@yahoo.com>
2025-06-04 16:17:39 -0400

    bug

--- ansible/roles/nodejs_chat/files/app.js
+++ ansible/roles/nodejs_chat/files/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files like index.html from current dir
+app.use(express.static(__dirname));
+
+// Route to load index.html at root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Socket.IO connection handling
 io.on('connection', socket => {
     console.log('User connected');
     socket.on('disconnect', () => console.log('User disconnected'));
