b453623363afc449407bddf333557c783057c65d
Samuel A. Sowah <160193749+SASowah@users.noreply.github.com>
2025-06-09 17:14:01 -0400

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -26,6 +26,9 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
+  # Enable OIDDC provider for IAM roles
+  enable_irsa = true
+
   eks_managed_node_groups = {
     default = {
       instance_types = [var.node_instance_type]
