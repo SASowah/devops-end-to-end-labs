0985a8fed528e6f7783eda9717d6016d51e1cb1c
Samuel <samniisowah@yahoo.com>
2025-06-09 23:59:51 -0400

    update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -11,6 +11,17 @@ module "vpc" {
   single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
+
+    public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
+
   tags = {
     Project = "gitops"
   }
