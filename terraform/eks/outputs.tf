733bbc948ef6c785c022680152a7294ee153ddf3
Samuel A. Sowah <160193749+SASowah@users.noreply.github.com>
2025-06-12 21:45:12 -0400

    Delete terraform directory

--- terraform/eks/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_name" {
-  description = "Name of the EKS cluster"
-  value       = module.eks.cluster_id
-}
